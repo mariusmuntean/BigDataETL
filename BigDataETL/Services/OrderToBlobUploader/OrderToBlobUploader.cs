b48b35f1c60cc02d3063157a51b408e1c27ea076
Marius Muntean <marius.muntean@live.com>
2023-07-22 11:33:57 +0200

    Moved the JSON serialization to the stream to another class

--- BigDataETL/Services/OrderToBlobUploader/OrderToBlobUploader.cs
+++ BigDataETL/Services/OrderToBlobUploader/OrderToBlobUploader.cs
@@ -12,14 +12,20 @@ internal class OrderToBlobUploader : IOrderToBlobUploader
 {
     private readonly BlobContainerClient _blobContainerClient;
     private readonly JsonSerializerOptions _jsonSerializerOptions;
+    private readonly JsonWriterOptions _jsonWriterOptions;
 
     public OrderToBlobUploader(BlobContainerClient blobContainerClient)
     {
         _blobContainerClient = blobContainerClient;
-        _jsonSerializerOptions = new JsonSerializerOptions()
+        _jsonSerializerOptions = new JsonSerializerOptions
         {
             ReferenceHandler = ReferenceHandler.IgnoreCycles,
-            WriteIndented = true
+            WriteIndented = true,
+        };
+
+        _jsonWriterOptions = new JsonWriterOptions
+        {
+            Indented = true
         };
     }
 
@@ -66,20 +72,26 @@ internal class OrderToBlobUploader : IOrderToBlobUploader
 
     private async IAsyncEnumerable<Stream> GetOrderStreams(IAsyncEnumerable<Order> orders, int blockSize = 4 * 1_000_000)
     {
-        // ToDo: maybe use Span<byte> instead of copying so much memory around
-
+        var tempBuffer = new byte[blockSize];
         var currentStream = new MemoryStream();
+        var orderWriter = new OrderWriter();
+
+        // Start JSON content and add an 'orders' array
+        await orderWriter.Start(currentStream);
+
         await foreach (var order in orders)
         {
-            await JsonSerializer.SerializeAsync(currentStream, order, _jsonSerializerOptions); // ToDo: switch to Utf8JsonWriter
+            // Add current order to the stream
+            await orderWriter.WriteOrder(currentStream, order);
+
+            // While the stream is larger than the defined block size, return blockSize chunks from it.
             while (currentStream.Length > blockSize)
             {
                 currentStream.Seek(0, SeekOrigin.Begin);
 
                 // Return a new stream with blockSize bytes from the current stream
-                var destinationBuffer = new byte[blockSize];
-                var readByteCount = await currentStream.ReadAsync(destinationBuffer, 0, blockSize);
-                yield return new MemoryStream(destinationBuffer, 0, readByteCount);
+                var readByteCount = await currentStream.ReadAsync(tempBuffer, 0, blockSize);
+                yield return new MemoryStream(tempBuffer, 0, readByteCount);
 
                 // Copy the remaining bytes into a new stream and set that one as the current stream
                 var newStream = new MemoryStream();
@@ -88,11 +100,62 @@ internal class OrderToBlobUploader : IOrderToBlobUploader
             }
         }
 
+        // Close the JSON array property and object
+        await orderWriter.End(currentStream);
+
         // Return the last remaining bytes from the stream, if any
         if (currentStream.Length > 0)
         {
             currentStream.Seek(0, SeekOrigin.Begin);
             yield return currentStream;
         }
+    }
+}
+
+internal class OrderWriter
+{
+    private int _writtenOrdersCount;
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+    public OrderWriter()
+    {
+        _writtenOrdersCount = 0;
+        _jsonSerializerOptions = new JsonSerializerOptions
+        {
+            ReferenceHandler = ReferenceHandler.IgnoreCycles,
+            WriteIndented = true,
+        };
+    }
+
+    public async Task Start(Stream stream)
+    {
+        // Start JSON object and add an 'orders' array
+        var streamWriter = new StreamWriter(stream, Encoding.UTF8);
+        await streamWriter.WriteLineAsync("{");
+        await streamWriter.WriteLineAsync("\t\"orders\": [");
+        await streamWriter.FlushAsync();
+    }
+
+    public async Task WriteOrder(Stream currentStream, Order order)
+    {
+        // Except for the first order, write a comma before each order.
+        if (_writtenOrdersCount > 0)
+        {
+            var streamWriter = new StreamWriter(currentStream, Encoding.UTF8);
+            await streamWriter.WriteLineAsync(",");
+            await streamWriter.FlushAsync();
+        }
+
+        await JsonSerializer.SerializeAsync(currentStream, order, _jsonSerializerOptions);
+        _writtenOrdersCount++;
+    }
+
+    public async Task End(Stream stream)
+    {
+        // Close the JSON array property and object
+        var streamWriter = new StreamWriter(stream, Encoding.UTF8);
+        await streamWriter.WriteLineAsync("\n\t ]");
+        await streamWriter.WriteLineAsync("}");
+        await streamWriter.FlushAsync();
     }
 }
